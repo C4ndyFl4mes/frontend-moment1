c39568b4b966624facef2d8299ff531301aa3c9d
Isaac Svedin <swedinisaac@gmail.com>
2025-01-21 18:50:39 +0100

    Updates

---
+++ index.c2db0733.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("menu"),n=document.getElementById("open-menu-btn"),t=document.getElementById("close-menu-btn");function l(){"none"===window.getComputedStyle(e).display?(e.style.display="block",t.style.display="block",n.style.display="none"):(e.style.display="none",t.style.display="none",n.style.display="block")}n.addEventListener("click",l),t.addEventListener("click",l);
+//# sourceMappingURL=index.c2db0733.js.map
